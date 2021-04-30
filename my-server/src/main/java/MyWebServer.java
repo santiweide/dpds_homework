4d9eab3fa8986c8f3ecf03998737a2f86a5fc8d7
santiweide <ggy_778@sina.com>
2021-04-30 20:24:03 +0800

    basic func

--- my-server/src/main/java/MyWebServer.java
+++ my-server/src/main/java/MyWebServer.java
@@ -1,9 +1,11 @@
 
 import utils.MyConsts;
+import utils.ServerHelper;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 class Worker extends Thread {
     Socket sock;
@@ -14,7 +16,77 @@ class Worker extends Thread {
 
     @Override
     public void run() {
+        String wwwhome = "D:\\test_src";
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            OutputStream out = new BufferedOutputStream(sock.getOutputStream());
+            PrintStream pout = new PrintStream(out);
 
+            // read first line of request (ignore the rest)
+            String request = in.readLine();
+            if (request == null) {
+                return;
+            }
+            ServerHelper.log(sock, request);
+//            while (true) {
+//                String misc = in.readLine();
+//                if (misc == null || misc.length() == 0) {
+//                    break;
+//                }
+//            }
+            // parse the line
+            boolean GETRequest = request.startsWith("GET");
+            boolean isHttp1 = request.endsWith("HTTP/1.0") || request.endsWith("HTTP/1.1");
+            if (!GETRequest || request.length() < 14 || !isHttp1) {
+                // bad request
+                ServerHelper.errorReport(pout, sock, "400", "Bad Request",
+                        "Your browser sent a request that " +
+                                "this server could not understand.");
+            } else {
+                String req = request.substring(4, request.length() - 9).trim();
+                if (req.contains("..") ||
+                        req.contains("/.ht") || req.endsWith("~")) {
+                    // evil hacker trying to read non-wwwhome or secret file
+                    ServerHelper.errorReport(pout, sock, "403", "Forbidden",
+                            "You don't have permission to access the requested URL.");
+                } else {
+                    String path = wwwhome + "\\" + req;
+                    File f = new File(path);
+                    if (f.isDirectory() && !path.endsWith("/")) {
+                        // redirect browser if referring to directory without final '/'
+                        pout.print("HTTP/1.0 301 Moved Permanently\r\n" +
+                                "Location: http://" +
+                                sock.getLocalAddress().getHostAddress() + ":" +
+                                sock.getLocalPort() + "/" + req + "/\r\n\r\n");
+                        ServerHelper.log(sock, "301 Moved Permanently");
+                    } else {
+                        if (f.isDirectory()) {
+                            // if directory, implicitly add 'index.html'
+                            path = path + "index.html";
+                            f = new File(path);
+                        }
+                        try {
+                            // send file
+                            InputStream file = new FileInputStream(f);
+                            pout.print("HTTP/1.0 200 OK\r\n" +
+                                    "Content-Type: " + ServerHelper.guessContentType(path) + "\r\n" +
+                                    "Date: " + new Date() + "\r\n" +
+                                    "Server: FileServer 1.0\r\n\r\n");
+                            // send raw file
+                            ServerHelper.sendFile(file, out);
+                            ServerHelper.log(sock, "200 OK");
+                        } catch (FileNotFoundException e) {
+                            // file not found
+                            ServerHelper.errorReport(pout, sock, "404", "Not Found",
+                                    "The requested URL was not found on this server.");
+                        }
+                    }
+                }
+            }
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
@@ -28,11 +100,11 @@ public class MyWebServer {
 
         ServerSocket serverSocket = new ServerSocket(MyConsts.DEFAULT_IP_PORT, MyConsts.QUEUE_LENGTH);
         while (true) {
-            try (Socket socket = serverSocket.accept()) {
+            try {
+                Socket socket = serverSocket.accept();
                 new Worker(socket).start();
             } catch (Exception e) {
                 e.printStackTrace();
-                break;
             }
         }
     }
---
+++ my-server/src/main/java/utils/ServerHelper.java
@@ -0,0 +1,60 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @author algorithm
+ */
+public class ServerHelper {
+    public static void log(Socket connection, String msg) {
+        System.err.println(new Date() + " [" + connection.getInetAddress().getHostAddress() +
+                ":" + connection.getPort() + "] " + msg);
+    }
+
+    public static void errorReport(PrintStream pout, Socket connection,
+                                    String code, String title, String msg) {
+        pout.print("HTTP/1.0 " + code + " " + title + "\r\n" +
+                "\r\n" +
+                "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\r\n" +
+                "<TITLE>" + code + " " + title + "</TITLE>\r\n" +
+                "</HEAD><BODY>\r\n" +
+                "<H1>" + title + "</H1>\r\n" + msg + "<P>\r\n" +
+                "<HR><ADDRESS>FileServer 1.0 at " +
+                connection.getLocalAddress().getHostName() +
+                " Port " + connection.getLocalPort() + "</ADDRESS>\r\n" +
+                "</BODY></HTML>\r\n");
+        log(connection, code + " " + title);
+    }
+
+    public static String guessContentType(String path) {
+        if (path.endsWith(".html") || path.endsWith(".htm")) {
+            return "text/html";
+        } else if (path.endsWith(".txt") || path.endsWith(".java")) {
+            return "text/plain";
+        } else if (path.endsWith(".gif")) {
+            return "image/gif";
+        } else if (path.endsWith(".class")) {
+            return "application/octet-stream";
+        } else if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
+            return "image/jpeg";
+        } else {
+            return "text/plain";
+        }
+    }
+
+    public static void sendFile(InputStream file, OutputStream out) {
+        try {
+            byte[] buffer = new byte[1000];
+            while (file.available() > 0) {
+                out.write(buffer, 0, file.read(buffer));
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
