8e21fd6fce4ccc829549c408b86218a7cf0955e7
santiweide <ggy_778@sina.com>
2021-05-30 13:51:33 +0800

    Async

--- async-joke/src/main/java/com/dut/joke/async/AsyncJokeClient.java
+++ async-joke/src/main/java/com/dut/joke/async/AsyncJokeClient.java
@@ -4,6 +4,7 @@ package com.dut.joke.async;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 /**
@@ -12,36 +13,39 @@ import java.util.Scanner;
 public class AsyncJokeClient {
     public static void main(String[] args) {
 
-        System.out.println("ASync Santiweide's Joke Client, 0.1.\n");
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            System.out.print("Press Return for joke or proverb: ");
-            System.out.print("Getting a joke or proverb from server... ");
-            System.out.flush();
-            System.out.println(getData());
-            System.out.flush();
-            System.out.println("Enter numbers to sum: ");
-            Integer a= scanner.nextInt();
-            Integer b= scanner.nextInt();
-            System.out.println("Your Sum is: " + (a + b));
-            System.out.flush();
-        }
-    }
-
-
-    static String getData() {
-        String received = null;
         try {
-            DatagramSocket socket = new DatagramSocket();;
-            DatagramPacket packet;
-            byte[] buf = new byte[255];
+            DatagramSocket socket = new DatagramSocket(0);
+
+            System.out.println("ASync Santiweide's Joke Client, 0.1.\n");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Press Return for joke or proverb: ");
+            System.out.flush();
+
+            String str = scanner.nextLine();
+            Integer port = socket.getLocalPort();
+            System.out.println("'Client port: "+ port);
+            byte[] buf = port.toString().getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName("localhost"), 4945);
+            socket.send(packet);
+
+            System.out.println("Getting a joke or proverb from server... ");
             packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
-            received = new String(packet.getData(), 0, packet.getLength());
-        } catch (IOException x) {
-            System.out.println("Socket error.");
-            x.printStackTrace();
+
+            String received = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Get from server: " + received);
+            System.out.flush();
+
+            System.out.println("Enter numbers to sum: ");
+            Integer a = scanner.nextInt();
+            Integer b = scanner.nextInt();
+            System.out.println("Your Sum is: " + (a + b));
+            System.out.flush();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return received;
+
     }
+
 }
--- async-joke/src/main/java/com/dut/joke/async/AsyncJokeServer.java
+++ async-joke/src/main/java/com/dut/joke/async/AsyncJokeServer.java
@@ -1,8 +1,100 @@
 package com.dut.joke.async;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+class WorkerData {
+    private List<Integer> list = Arrays.asList(0, 1, 2, 3);
+    private Integer pos = 0;
+    private boolean first = true;
+    private Map<Integer, Character> _map = new ConcurrentHashMap<>();
+
+    WorkerData() {
+        for (int i = 0; i < 4; i++) {
+            _map.put(i, (char) (i + 'A'));
+        }
+    }
+
+    private void randomize() {
+        Collections.shuffle(list);
+    }
+
+    public Character getNextChar() {
+        Character ret;
+        if (pos == 0) {
+            if (first) {
+                first = false;
+            } else {
+                System.out.println((AsyncJokeServer.serverMode ? "PROVERB" : "JOKE") + " CYCLE COMPLETED");
+            }
+        }
+        ret = _map.get(list.get(pos));
+        pos = (pos + 1) % list.size();
+        if (pos == 0) {
+            randomize();
+        }
+        return ret;
+    }
+
+}
+
+/**
+ * 一个Worker Handle一个 port
+ */
+class Worker extends Thread {
+    @Override
+    public void run() {
+        try {
+            DatagramSocket sock = null;
+            while (AsyncJokeServer.running) {
+
+                sock = new DatagramSocket(4945);
+                byte[] buf = new byte[256];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                sock.receive(packet);
+                String clientUDPServerPortStr = new String(packet.getData(), 0, packet.getLength());
+                Integer clientPort = Integer.valueOf(clientUDPServerPortStr);
+                System.out.println("'Server receive client port: "+ clientPort);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(AsyncJokeServer.serverMode ? "P" : "J");
+                stringBuilder.append(AsyncJokeServer.socketDataMap.get(" ").getNextChar());
+                stringBuilder.append(" ");
+                String msg = stringBuilder.toString();
+                System.out.println("Giving back "+ msg);
+                buf = msg.getBytes();
+                packet = new DatagramPacket(buf, buf.length, InetAddress.getByName("localhost"), clientPort);
+                Thread.sleep(1000);
+                sock.send(packet);
+            }
+            if(sock != null){
+                sock.close();
+            }
+        } catch (IOException | InterruptedException x) {
+            System.out.println("Server read client name error");
+            x.printStackTrace();
+        }
+    }
+}
+
 /**
  * @author algorithm
  */
 public class AsyncJokeServer {
+    /**
+     * 0: Joke
+     * 1: Proverb
+     */
+    public static boolean running = true;
+    public static boolean serverMode = false;
+    public static Map<String, WorkerData> socketDataMap = new ConcurrentHashMap<>();
 
+    public static void main(String[] args) {
+        socketDataMap.put(" ", new WorkerData());
+        System.out.println("Starting Santiweide Aysnc Joke Server");
+        new Thread(new Worker()).start();
+
+    }
 }
